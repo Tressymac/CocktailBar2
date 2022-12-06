95b8a2d9dc2d9c171911380cd4b1c71c9963d05a
Jessica <jessicamac48@yahoo.com>
2022-12-06 12:49:23 -0500

    Updates

--- src/components/ingredient.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Ingredient = ({unit, amount, ingredient}) => {
-    return(
-        <p className="card-text">{amount} {unit} of {ingredient} </p>
-    )
-}
-
-export default Ingredient;
